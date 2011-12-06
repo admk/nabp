fa20b32b087b8e799ec931f9c477635637166217
Xitong Gao <gxtfmx@gmail.com>
2011-12-06 21:15:28 +0000

    Fix 45 degrees discontinuity

--- MATLAB/nabp_configure.m
+++ MATLAB/nabp_configure.m
@@ -3,18 +3,18 @@ function nabp_cfg = nabp_configure(projection, projection_angles)
     % projection size
     nabp_cfg.p_line_size = size(projection, 1);
     nabp_cfg.p_angle_size = length(projection_angles);
-    nabp_cfg.p_line_center = (nabp_cfg.p_line_size + 1) / 2;
+    nabp_cfg.p_line_center = (nabp_cfg.p_line_size - 1) / 2;
     assert(nabp_cfg.p_angle_size == size(projection, 2), ...
         'Number of angles mismatch');
 
     % image size & center
     nabp_cfg.i_size = ceil(nabp_cfg.p_line_size / sqrt(2));
     half_i_size = nabp_cfg.i_size / 2;
-    if half_i_size ~= floor(half_i_size)
-        % ensure image size is even
+    if half_i_size == floor(half_i_size)
+        % ensure image size is odd
         nabp_cfg.i_size = nabp_cfg.i_size + 1;
     end
-    nabp_cfg.i_center = nabp_cfg.i_size / 2;
+    nabp_cfg.i_center = (nabp_cfg.i_size - 1) / 2;
 
     % number of partitions
     nabp_cfg.desired_no_of_partitions = 1;
