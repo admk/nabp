a312d0d6fe3223acc6dea18f0c7ea7084c9d83d5
Xitong Gao <gxtfmx@gmail.com>
2012-01-01 23:38:46 +0000

    Minor changes

--- src/pynabp/nabp_enums.py
+++ src/pynabp/nabp_enums.py
@@ -1,21 +1,23 @@
-class sector:
+from nabp_utils import enum
+
+class sector(enum):
     a = '2\'d0'
     b = '2\'d1'
     c = '2\'d2'
     d = '2\'d3'
 
-class scan_mode:
+class scan_mode(enum):
     x = '0'
     y = '1'
 
-class scan_direction:
+class scan_direction(enum):
     forward = '0'
     backward = '1'
 
-class buff_step_mode:
+class buff_step_mode(enum):
     tan = '0'
     cot = '1'
 
-class buff_step_direction:
+class buff_step_direction(enum):
     ascending = '0'
     descending = '1'
