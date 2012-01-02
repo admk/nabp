317ef5b70a8bffa9943ea905e8745c780a8df41c
Xitong Gao <gxtfmx@gmail.com>
2012-01-02 19:32:19 +0000

    Add accu and accu_base fixed point variables

--- src/shifter.v
+++ src/shifter.v
@@ -7,11 +7,14 @@
     from pynabp.nabp_config import nabp_config
     from pynabp.nabp_enums import shifter_states
     from pynabp.nabp_utils import bin_width_of_dec, dec_repr
+    from pynabp.fixed_point_arith import FixedPoint
 #}
+`define kAngleLength {# nabp_config()['kAngleLength'] #}
 
 module NABPShifter
 (
     // inputs from state_control
+    input wire [`kAngleLength-1:0] sc_angle,
     input wire sc_fill_kick,
     input wire sc_shift_kick,
     // outputs to state_control
@@ -20,18 +23,30 @@ module NABPShifter
     // outputs to filter mapper
     output reg fm_shift_enable
 );
-
 {#
     fill_cnt_init = nabp_config()['partition_scheme']['partitions'][-1] + 1
     fill_cnt_width = bin_width_of_dec(fill_cnt_init)
+
     shift_cnt_init = nabp_config()['image_size']
     shift_cnt_width = bin_width_of_dec(shift_cnt_init)
+
+    accu_max = nabp_config()['image_size']
+    accu_int_width = bin_width_of_dec(accu_max)
+    accu_frac_width = 3
+    accu_fixed = FixedPoint(accu_int_width, accu_frac_width, value=0)
+
+    accu_base_int_width = 1
+    accu_base_frac_width = 3
+    accu_base_fixed = FixedPoint(
+            accu_base_int_width, accu_base_frac_width, value=0)
 #}
 reg unsigned [{# fill_cnt_width - 1 #}:0] fill_cnt;
 reg unsigned [{# shift_cnt_width - 1 #}:0] shift_cnt;
+reg {# accu_fixed.verilog_decl() #} accu;
+reg {# accu_base_fixed.verilog_decl() #} accu_base;
 
 always @(posedge clk)
-begin:cnters
+begin:counters
     if (state == fill_s and fill_cnt != 0)
         fill_cnt <= fill_cnt - 1;
     else
@@ -40,6 +55,13 @@ begin:cnters
         shift_cnt <= shift_cnt - 1;
     else
         shift_cnt <= {# dec_repr(shift_cnt_init) #};
+    if (state == shift_s)
+        accu <= accu + accu_base;
+    else
+    begin
+        accu <= 0'b{# accu_fixed.bin_repr_of_value(0) #};
+        accu_base <= // TODO lookup table for accu_base
+    end
 end
 
 {# include('templates/state_decl(states).v', states=shifter_states) #}
