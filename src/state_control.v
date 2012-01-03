c082cc26083a0c5986f62693c433ae038f759d17
Xitong Gao <gxtfmx@gmail.com>
2012-01-03 02:53:53 +0000

    state_ctrl and swap_ctrl to share angle & line_cnt

--- src/state_control.v
+++ src/state_control.v
@@ -3,11 +3,16 @@
 //     31 Dec 2011
 // Provides system states for the NABP architecture
 {#
-from pynabp.enums import state_control_states
-from pynabp.conf import conf
-a_len = conf()['kAngleLength']
+    from pynabp.utils import bin_width_of_dec, dec_repr
+    from pynabp.enums import state_control_states
+    from pynabp.conf import conf
+
+    a_len = conf()['kAngleLength']
+    pe_width = conf()['partition_scheme']['size']
+    pe_width_len = bin_width_of_dec(pe_width)
 #}
 `define kAngleLength {# a_len #}
+`define kPEWidthLength {# pe_width_len #}
 
 module NABPStateControl
 (
@@ -15,16 +20,18 @@ module NABPStateControl
     input wire clk,
     input wire reset_n,
     // inputs from swap control
+    input wire [`kPEWidthLength-1:0] sw_line_cnt,
     input wire [`kAngleLength-1:0] sw_angle,
     input wire sw_swap,
     // inputs from shifter
     input wire sh_fill_done,
     input wire sh_shift_done,
-    // output the angle this state control holds
+    // output the iteration data this state control holds
+    output reg [`kPEWidthLength-1:0] line_cnt,
     output reg [`kAngleLength-1:0] angle,
     // output to swap control
     output wire sw_swap_ready,
-    output wire sw_next_angle,
+    output wire sw_next_itr,
     // output to shifter
     output wire sh_fill_kick,
     output wire sh_shift_kick
@@ -42,14 +49,17 @@ begin:transition
     else
     begin
         if (state == setup_s)
+        begin
             angle <= sw_angle;
+            line_cnt <= sw_line_cnt;
+        end
         state <= next_state;
     end
 end
 
 // mealy outputs
 assign sw_swap_ready = (state == fill_done_s);
-assign sw_next_angle = (state == init_s) or
+assign sw_next_itr   = (state == init_s) or
                        (state == shift_done_s);
 assign sh_fill_kick  = (next_state != state) and
                        (next_state == fill_s);
