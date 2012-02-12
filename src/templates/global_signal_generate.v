77cce2af72fdb5d429fa15d646f82eaa7ea1e133
Xitong Gao <gxtfmx@gmail.com>
2012-02-12 21:59:24 +0000

    Cleaner global signal generate

--- src/templates/global_signal_generate.v
+++ src/templates/global_signal_generate.v
@@ -1,19 +1,19 @@
 {# from pynabp.conf import conf #}
 // clock & reset generate
 parameter clk_period = {# conf()['clock_period'] #} / 2;
-parameter half_clk_period = clk_period / 2;
 reg clk, reset;
 wire reset_n;
 assign reset_n = !reset;
 
+always
+begin:clk_generate
+    clk = 1'b0; #clk_period;
+    clk = 1'b1; #clk_period;
+end
+
 initial
-begin:global_signal_generate
-	clk = 1'b0; reset = 1'b1; #clk_period;
-	clk = 1'b1; #clk_period; clk = 1'b0; #clk_period;
-	clk = 1'b1; #half_clk_period; reset = 1'b0; #half_clk_period;
-	forever
-	begin
-		clk = 1'b0; #clk_period;
-		clk = 1'b1; #clk_period;
-	end
+begin:reset_generate
+	reset = 1'b1;
+	#(3.5*clk_period);
+	reset = 1'b0;
 end
