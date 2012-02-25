525cf4f549e392e02f482fe3a15aff8c502f7119
Xitong Gao <gxtfmx@gmail.com>
2012-02-25 03:43:39 +0000

    Finally correct translation for fixed point arith

--- translate_process_generate.py
+++ translate_process_generate.py
@@ -2,26 +2,33 @@ import os
 from akpytemp import Template
 from akpytemp.template import code_gobble
 
-translate_process_template = code_gobble(
-        """
+translate_process_template = \
+        r"""
         #!/usr/bin/env python
-        # dirty hack for minimal code size
-        import sys
-        sys.path.append('../src/')
-        from pynabp.conf import conf
-        fixed = conf['{# fixed #}']
-        while True:
-            try:
-                fixed.value = int(raw_input())
-                val = fixed.value
-            except:
-                val = '?'
-            sys.stdout.write(val)
+        import sys, os
+        try:
+            sys.path.append(os.path.abspath('.'))
+            from pynabp.conf import conf
+            fixed = conf()['{# fixed #}']
+        except:
+            sys.stdout.write('!Preamble failed.\n')
             sys.stdout.flush()
-        """)
+        while True:
+            val_str = raw_input()
+            try:
+                fixed.value = val_str
+                val_str = str(fixed.value)
+            except Exception as e:
+                val_str = '!' + str(e) + ': ' + val_str
+            sys.stdout.write(val_str + '\n')
+            sys.stdout.flush()
+        """
+translate_process_template = code_gobble(
+        translate_process_template, gobble_count=None, eat_empty_lines=True)
 
 def write_translate_process(target):
     target_key = os.path.splitext(target)[0]
     target_key = os.path.split(target_key)[1]
     t = Template(template=translate_process_template)
-    t.save(target, fixed=target_key)
+    t.save(target, fixed=target_key, target=target)
+    os.chmod(target, 0755)
