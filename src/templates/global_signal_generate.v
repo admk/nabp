b2f2a5fd484e8383bb8d207acaea40e427d0a719
Xitong Gao <gxtfmx@gmail.com>
2012-02-26 03:45:30 +0000

    Refactor clk_period -> clk_delay

--- src/templates/global_signal_generate.v
+++ src/templates/global_signal_generate.v
@@ -1,21 +1,21 @@
 {# from pynabp.conf import conf #}
 // clock & reset generate
-parameter clk_period = {# conf()['clock_period'] #} / 2;
+parameter clk_delay = {# conf()['clock_period'] #} / 2;
 reg clk, reset;
 wire reset_n;
 assign reset_n = !reset;
 
 always
 begin:clk_generate
-    clk = 1'b0; #clk_period;
-    clk = 1'b1; #clk_period;
+    clk = 1'b0; #clk_delay;
+    clk = 1'b1; #clk_delay;
 end
 
 initial
 begin:reset_generate
-	reset = 1'b1;
-	#(3.5*clk_period);
-	reset = 1'b0;
+    reset = 1'b1;
+    #(3.5*clk_delay);
+    reset = 1'b0;
 end
 
 // simulation time limit
