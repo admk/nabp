040b4ba750210d0aaf61ca899d18dd346867dbe2
Xitong Gao <gxtfmx@gmail.com>
2012-02-27 01:24:41 +0000

    Refactor mapper_lut, shifter_lut

--- src/shifter_lut.v
+++ src/shifter_lut.v
@@ -1,25 +1,20 @@
-{# include('templates/info.v') #}
+{# include('templates/defines.v') #}
 // NABPShifterLUT
 //     1 Jan 2012
 // Provides NABPShifter the look-up values of $\tan\theta$ and $\cot\theta$
 // TODO reduce look-up table size by using symmetry
 {#
-    from pynabp.utils import xfrange, dec_repr
-    from pynabp.conf import conf
-    from pynabp.fixed_point_arith import FixedPoint
+    from pynabp.utils import xfrange
     from math import tan, radians
 
-    a_len = conf()['kAngleLength']
-    accu_fixed = conf()['tShiftAccuBase']
-    lookup = conf()['lutShiftAccuBaseFunc']
+    lookup = c['lutShiftAccuBaseFunc']
 #}
-`define kAngleLength {# a_len #}
 
 module NABPShifterLUT
 (
     input wire clk,
     input wire [`kAngleLength-1:0] sh_angle,
-    output reg {# accu_fixed.verilog_decl() #} sh_accu_base
+    output reg {# conf()['tShiftAccuBase'].verilog_decl() #} sh_accu_base
 );
 
 always @(posedge clk)
@@ -27,7 +22,7 @@ begin
     {# set_eat_blanklines(True) #}
     case (sh_angle)
         {% for angle in xfrange(0, 180, conf()['projection_angle_step']) %}
-        {# dec_repr(angle, a_len) #}:
+        {# dec_repr(angle, conf()['kAngleLength']) #}:
             sh_accu_base <= {# lookup(angle) #};
         {% end %}
     endcase
