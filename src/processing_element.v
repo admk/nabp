ca8513db4e22ee17cf1590efa07f515a16970146
Xitong Gao <gxtfmx@gmail.com>
2012-02-27 01:28:24 +0000

    And refactor processing_element

--- src/processing_element.v
+++ src/processing_element.v
@@ -1,4 +1,4 @@
-{# include('templates/info.v') #}
+{# include('templates/defines.v') #}
 // NABPProcessingElement
 //     10 Jan 2011
 // Processing element
@@ -33,30 +33,20 @@
 // example cache no. 5 stores the diagonal blocks of the image.)
 
 {#
-    from pynabp.conf import conf
     from pynabp.enums import scan_mode
-    from pynabp.utils import bin_width_of_dec, dec_repr
 
-    data_len = conf()['kFilteredDataLength']
-    partition_size = conf()['partition_scheme']['size']
-    partition_size_len = bin_width_of_dec(partition_size)
-    no_partitions = conf()['partition_scheme']['no_of_partitions']
-    no_partitions_len = bin_width_of_dec(no_partitions)
+    partition_size = c['partition_scheme']['size']
+    partition_size_len = bin_width(partition_size)
+    no_partitions = c['partition_scheme']['no_of_partitions']
+    no_partitions_len = bin_width(c['partition_scheme']['no_of_partitions'])
     addr_len = partition_size_len * 2 + no_partitions_len
-    image_size_len = bin_width_of_dec(conf()['image_size'])
 
-    def to_v(val):
+    def to_l(val):
         return dec_repr(val, partition_size_len)
     def to_sel(val):
         return dec_repr(val, no_partitions_len)
 #}
-
-`define kNoPartitions {# no_partitions #}
-`define kNoPartitonsLength {# no_partitions_len #}
-`define kFilteredDataLength {# data_len #}
-`define kPartitionSizeLength {# partition_size_len #}
 `define kAddressLength {# addr_len #}
-`define kImageSizeLength {# image_size_len #}
 
 module NABPProcessingElement
 (
@@ -73,20 +63,20 @@ module NABPProcessingElement
     // inputs from cache control
     input wire signed [`kFilteredDataLength-1:0] cc_read_val,
     // outputs to cache control
-    output reg unsigned [`kNoPartitonsLength-1:0] cc_sel,
+    output reg unsigned [`kNoOfPartitonsLength-1:0] cc_sel,
     output wire [`kAddressLength-1:0] cc_read_addr,
     output reg cc_write_en,
     output reg [`kAddressLength-1:0] cc_write_addr,
     output reg signed [`kFilteredDataLength-1:0] cc_write_val
 );
 
-parameter [`kNoPartitonsLength-1:0] pe_id = 'bz;
+parameter [`kNoOfPartitonsLength-1:0] pe_id = 'bz;
 parameter [`kImageSizeLength-1:0] pe_tap = 'bz;
 
 reg unsigned [`kPartitionSizeLength-1:0] line_itr;
 reg unsigned [`kPartitionSizeLength-1:0] scan_itr;
-reg unsigned [`kNoPartitonsLength-1:0] scan_sec;
-wire [`kNoPartitonsLength-1:0] cc_sec_sel;
+reg unsigned [`kNoOfPartitonsLength-1:0] scan_sec;
+wire [`kNoOfPartitonsLength-1:0] cc_sec_sel;
 assign cc_sec_sel = (sw_scan_mode == {# scan_mode.y #}) ? pe_id : scan_sec;
 assign cc_read_addr = (sw_scan_mode == {# scan_mode.x #} ? 
                       {cc_sec_sel, line_itr, scan_itr} :
@@ -96,23 +86,23 @@ always @(posedge clk)
 begin:counter
     if (sw_reset)
     begin
-        line_itr <= {# to_v(0) #};
-        scan_itr <= {# to_v(0) #};
+        line_itr <= {# to_l(0) #};
+        scan_itr <= {# to_l(0) #};
         scan_sec <= {# to_sel(0) #};
         cc_sel <= pe_id;
     end
     else if (sw_kick)
     begin
-        line_itr <= line_itr + {# to_v(1) #};
-        scan_itr <= {# to_v(0) #};
+        line_itr <= line_itr + {# to_l(1) #};
+        scan_itr <= {# to_l(0) #};
         scan_sec <= {# to_sel(0) #};
         cc_sel <= pe_id;
     end
     else if (sw_en)
     begin
-        if (scan_itr == {# to_v(partition_size - 1) #})
+        if (scan_itr == {# to_l(partition_size - 1) #})
         begin
-            scan_itr <= {# to_v(0) #};
+            scan_itr <= {# to_l(0) #};
             scan_sec <= scan_sec + {# to_sel(1) #};
             if (cc_sel == {# to_sel(no_partitions - 1) #})
                 cc_sel <= {# to_sel(0) #};
@@ -129,7 +119,7 @@ begin:cc_write
     cc_write_val <= cc_read_val + lb_val;
 end
 
-{% if conf()['debug'] %}
+{% if c['debug'] %}
 integer file, err;
 reg [20*8:1] file_name;
 reg unsigned [`kImageSizeLength-1:0] im_x, im_y, scan_pos, line_pos;
@@ -141,8 +131,9 @@ begin
     always @(posedge clk)
         if (cc_write_en)
         begin
-            scan_pos = {# dec_repr(partition_size, image_size_len) #} *
-                    scan_sec + scan_itr;
+            scan_pos = {#
+                    dec_repr(partition_size, bin_width(c['image_size']))
+                #} * scan_sec + scan_itr;
             line_pos = pe_tap + line_itr;
             if (sw_scan_mode == {# scan_mode.x #})
             begin
