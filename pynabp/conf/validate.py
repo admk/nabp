5896c1403db920676979fe8bde37ab2e116d5c4d
Xitong Gao <gxtfmx@gmail.com>
2012-03-03 01:30:28 +0000

    Add preliminary constraints validation in validate

--- pynabp/conf/validate.py
+++ pynabp/conf/validate.py
@@ -1,35 +1,33 @@
-class Validator(object):
-    """A simple validator class
+from validator.base import Validator
+from validator.constraints import ConstraintsValidator, func, \
+        natural, positive, odd, even, time_unit, function_arg_count
 
-    Should be subclassed to implement validators.
-    """
 
+class ValidatorCollate(Validator, ConstraintsValidator):
     def __init__(self, config):
-        super(Validator, self).__init__()
-        self._validate()
-
-    def _validation_methods(self):
-        """Find all validation methods within the class
-
-        Returns - a list of methods that can be called with the config file as
-        the parameter for each method.
-        """
-        import inspect
-        methods = [member[0] for member in inspect.getmembers(
-                self.__class__, predicate=inspect.ismethod)]
-        return [getattr(self, method)
-                for method in methods if method.startswith('validate')]
-
-    def _validate(self, config):
-        """Validate cofig by calling each validation method in turn with the
-        config
-
-        If failed validation method should raise an exception.
-        """
-        for validation_method in self._validation_methods():
-            try:
-                validation_method(config)
-            except:
-                print 'Validation failed in method ' + \
-                        str(validation_method)
-                raise
+        super(ValidatorCollate, self).__init__(config)
+        constraints = {
+                    # key,               type,  None,   constraint functions
+                    'debug':            (bool,  False,  None),
+                    'clock_period':     (int,   False,  positive),
+                    'time_precision':   (str,   False,  time_unit),
+                    'device':           (str,   True,   None),
+                    'projection_line_size':
+                                        (int,   False,  [odd, positive]),
+                    'image_size':       (int,   True,   [odd, positive]),
+                    'fir_order':        (int,   False,  [even, positive]),
+                    'fir_function':     (func,  True,   function_arg_count(1)),
+                    'no_of_processing_elements':
+                                        (int,   False,  positive),
+                    'kDataLength':      (int,   False,  positive),
+                    'kFilteredDataPrecision':
+                                        (int,   False,  natural),
+                    'kAnglePrecision':  (int,   False,  natural),
+                    'kShiftAccuPrecision':
+                                        (int,   False,  positive),
+                    'kMapAccuPrecision':
+                                        (int,   False,  positive),
+                    'kMapAccuPartPrecision':
+                                        (int,   False,  positive),
+                }
+        self.add_constraints(constraints)
