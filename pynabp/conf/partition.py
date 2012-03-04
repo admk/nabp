026f09e46240be43da0ec87e060c22c4bd6d06ad
Xitong Gao <gxtfmx@gmail.com>
2012-03-04 02:55:02 +0000

    Refactor partitioning validator

--- pynabp/conf/partition.py
+++ pynabp/conf/partition.py
@@ -1,32 +1,60 @@
 import math
 
+
+def _validate(partition_function):
+    """Encapsulate a validator to use as a decorator"""
+
+    def validator(image_size, no_of_partitions):
+        """A general validator for partitioning algorithms"""
+
+        # generate scheme with the partition function
+        scheme = partition_function(image_size, no_of_partitions)
+
+        # check for partitioning efficiency
+        if scheme['no_of_partitions'] > image_size:
+            raise ValueError(
+                    'no_of_partitions should not be greater than image size')
+
+        # check last tap for consistencies
+        last_tap = (scheme['no_of_partitions'] - 1) * scheme['size']
+        if last_tap != scheme['partitions'][-1]:
+            raise ValueError('Last tap and partitions disagree')
+
+        # check coverage for consistencies
+        wasted_pixels = int(last_tap + scheme['size'] - image_size)
+        if wasted_pixels < 0:
+            raise ValueError('Partitions does not cover the entire image size')
+
+        return scheme
+
+    return validator
+
+
+@_validate
 def partition(image_size, no_of_partitions):
     """
     >>> partition(512, 3)
-    {'wasted_pixels': 1, 'partitions': [0, 171, 342], 'no_of_partitions': 3, 'size': 171}
+    {'partitions': [0, 171, 342], 'no_of_partitions': 3, 'size': 171}
     >>> partition(512, 4)
-    {'wasted_pixels': 0, 'partitions': [0, 128, 256, 384], 'no_of_partitions': 4, 'size': 128}
+    {'partitions': [0, 128, 256, 384], 'no_of_partitions': 4, 'size': 128}
     >>> partition(512, 5)
-    {'wasted_pixels': 3, 'partitions': [0, 103, 206, 309, 412], 'no_of_partitions': 5, 'size': 103}
+    {'partitions': [0, 103, 206, 309, 412], 'no_of_partitions': 5, 'size': 103}
     """
-    partition_scheme = {}
-    partition_scheme['no_of_partitions'] = int(no_of_partitions)
-    partition_scheme['size'] = int(math.ceil(
+    partition_size = int(math.ceil(
             image_size / float(no_of_partitions)))
-    partition_scheme['partitions'] = []
-    for idx in xrange(no_of_partitions):
-        offset = int(idx * partition_scheme['size'])
-        partition_scheme['partitions'].append(offset)
-    # check for consistencies
-    last_tap = (no_of_partitions - 1) * partition_scheme['size']
-    if last_tap != partition_scheme['partitions'][-1]:
-        raise Exception('Last tap and partitions disagree')
-    partition_scheme['wasted_pixels'] = int(last_tap +
-            partition_scheme['size'] - image_size)
-    if partition_scheme['wasted_pixels'] < 0:
-        raise Exception('Partitions does not cover the entire image size')
+
+    partition_scheme = {
+            'no_of_partitions': int(no_of_partitions),
+            'size': partition_size,
+            'partitions':
+                    [int(idx * partition_size)
+                        for idx in xrange(no_of_partitions)],
+            }
+
     return partition_scheme
+
 
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
+    print partition(512, 3)
