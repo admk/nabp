a7702670f38fcee8a9ccccb9a25e20922d653142
Xitong Gao <gxtfmx@gmail.com>
2012-05-29 15:05:46 +0100

    Refactor test.py and add naming support

--- pynabp/test.py
+++ pynabp/test.py
@@ -1,32 +1,34 @@
 import numpy
 from PIL import Image
 
-_test_results = None
+_image = None
+_name = None
 
 
 def image(results):
-    i = Image.new('L', _test_results.shape)
+    i = Image.new('L', _image.shape)
     for (x, y), val in numpy.ndenumerate(results):
         i.putpixel((x, y), val)
     return i
 
 
-def init(image_size):
-    global _test_results
-    _test_results = numpy.zeros((image_size, image_size))
+def init(name, size):
+    global _image, _name
+    _image = numpy.zeros((size, size))
+    _name = name
 
 
 def update(x, y, val):
-    global _test_results
-    _test_results[x, y] += val
+    global _image
+    _image[x, y] += val
 
 
 def finish():
-    global _test_results
+    global _image, _name
     # normalise values to range 0~255
-    _test_results += _test_results.min()
-    _test_results *= 255.0 / _test_results.max()
+    _image += _image.min()
+    _image *= 255.0 / _image.max()
     # show image
-    result_image = image(_test_results)
-    result_image.save('build/result.png')
+    result_image = image(_image)
+    result_image.save('build/%s.png' % _name)
     result_image.show()
