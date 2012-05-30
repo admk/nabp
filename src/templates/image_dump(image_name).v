a85f9eacc49ba8dcd556ca3fc9f6c5b68ffafa9d
Xitong Gao <gxtfmx@gmail.com>
2012-05-30 02:13:58 +0100

    Further refactor pe_dump -> image_dump

--- src/templates/image_dump(image_name).v
+++ src/templates/image_dump(image_name).v
@@ -1,31 +1,31 @@
 {# from pynabp.conf_gen import config #}
 
-integer __pe_dump_err;
+integer __image_dump_err;
 
-task pe_dump_init;
+task image_dump_init;
     begin
         python_path_update();
-        __pe_dump_err = $pyeval("from pynabp import image_dump");
-        __pe_dump_err = $pyeval(
+        __image_dump_err = $pyeval("from pynabp import image_dump");
+        __image_dump_err = $pyeval(
                 "image_dump.init('{# image_name #}', ",
                 {# config['image_size'] #}, ")");
     end
 endtask
 
-task pe_dump_finish;
+task image_dump_finish;
     begin
-        __pe_dump_err = $pyeval("image_dump.finish('{# image_name #}')");
+        __image_dump_err = $pyeval("image_dump.finish('{# image_name #}')");
     end
 endtask
 
-task pe_dump_pixel;
+task image_dump_pixel;
     input integer x;
     input integer y;
     input integer val;
     begin
         if (x < {# config['image_size'] #} &&
             y < {# config['image_size'] #})
-            __pe_dump_err = $pyeval(
+            __image_dump_err = $pyeval(
                     "image_dump.update('{# image_name #}', ",
                     x, ",", y, ",", val, ")");
     end
