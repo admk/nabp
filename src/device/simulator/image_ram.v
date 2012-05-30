9b227db57f2f340706230f520931964b52f9224e
Xitong Gao <gxtfmx@gmail.com>
2012-05-30 02:01:16 +0100

    Add preliminary state machine for image_ram

--- src/device/simulator/image_ram.v
+++ src/device/simulator/image_ram.v
@@ -1,5 +1,10 @@
 {# include('templates/defines.v') #}
 // NABPImageRAM
+//     30 May 2012
+{#
+    from pynabp.enums import image_ram_states
+#}
+
 module NABPImageRAM
 (
     // global signals
@@ -14,6 +19,34 @@ module NABPImageRAM
     output wire ir_enable
 );
 
+{# include('templates/state_decl(states).v', states=image_ram_states()) #}
+
+// mealy outputs
+wire working;
+assign working = (state == work_s);
+
+// mealy next state
+always @(*)
+begin:next_state
+    next_state <= state;
+    case (state) // synopsys parallel_case full_case
+        ready_s:
+            if (ir_kick)
+                next_state <= work_s;
+        work_s:
+            if (ir_done)
+                next_state <= ready_s;
+    endcase
+end
+
+// mealy state transition
+always @(posedge clk)
+begin:transition
+    if (!reset_n)
+        state <= ready_s;
+    else
+        state <= next_state;
+end
 
 
 endmodule
