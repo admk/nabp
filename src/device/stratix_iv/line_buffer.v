286885b4786fdeae87ac300da88f30a8029ca534
Xitong Gao <gxtfmx@gmail.com>
2012-06-08 18:37:16 -0700

    Fix line_buffer for all no of PEs

--- src/device/stratix_iv/line_buffer.v
+++ src/device/stratix_iv/line_buffer.v
@@ -18,28 +18,60 @@ input wire clk, reset_n, enable;
 input wire [pDataLength-1:0] shift_in;
 output wire [pDataLength*pNoTaps-1:0] taps;
 
-reg signed [`kFilteredDataLength-1:0] tap_b1;
-wire [pDataLength*(pNoTaps-1)-1:0] taps_tp;
+reg signed [pDataLength-1:0] tap_b1;
 
 always @(posedge clk)
     if (enable)
         tap_b1 <= shift_in;
 
-assign taps = {taps_tp, tap_b1};
-
-altshift_taps
-#(
-    .NUMBER_OF_TAPS(pNoTaps-1),
-    .POWER_UP_STATE("DONT_CARE"),
-    .TAP_DISTANCE(pTapsWidth),
-    .WIDTH(pDataLength)
-)
-pe_line_buff
-(
-    .clken(enable),
-    .clock(clk),
-    .shiftin(tap_b1),
-    .taps(taps_tp)
-);
+genvar i, j;
+generate
+    if (pNoTaps == 1)
+    begin:gen_no_line_buff
+        assign taps = tap_b1;
+    end
+    else
+    begin:gen_line_buff
+        wire [pDataLength*(pNoTaps-1)-1:0] taps_tp;
+        assign taps = {taps_tp, tap_b1};
+        if (pTapsWidth >= 3)
+        begin:gen_altshift_taps
+            altshift_taps
+            #(
+                .NUMBER_OF_TAPS(pNoTaps-1),
+                .POWER_UP_STATE("DONT_CARE"),
+                .TAP_DISTANCE(pTapsWidth),
+                .WIDTH(pDataLength)
+            )
+            pe_line_buff
+            (
+                .clken(enable),
+                .clock(clk),
+                .shiftin(tap_b1),
+                .taps(taps_tp)
+            );
+        end
+        else
+        begin:gen_reg_shift_taps
+            reg [pDataLength-1:0] shift_regs[pNoTaps*pTapsWidth:0];
+            always @(posedge clk)
+                shift_regs[pTapsWidth] <= tap_b1;
+            for (i = 1; i < pNoTaps; i = i + 1)
+            begin:gen_taps
+                assign taps_tp[pDataLength*i-1:pDataLength*(i-1)] =
+                        shift_regs[(i+1)*pTapsWidth-1];
+            end
+            for (i = 1; i < pNoTaps; i = i + 1)
+            begin:gen_shift_regs_outer
+                for (j = 0; j < pTapsWidth; j = j + 1)
+                begin:gen_shift_regs_update
+                    always @(posedge clk)
+                        shift_regs[i * pTapsWidth + j + 1] <=
+                                shift_regs[i * pTapsWidth + j];
+                end
+            end
+        end
+    end
+endgenerate
 
 endmodule
