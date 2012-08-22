49ed2bae3e0f13c14a5a60f032487d289bd324e0
Xitong Gao <gxtfmx@gmail.com>
2012-08-22 22:36:34 +0800

    Fix shift register not implemented for width=1

--- src/device/generic/shift_register.v
+++ src/device/generic/shift_register.v
@@ -19,22 +19,46 @@ input wire clk, reset_n, enable;
 input wire [pDataLength-1:0] val_in;
 output reg [pDataLength-1:0] val_out;
 
-reg [pDataLength-1:0] data[pDelay-2:0];
-reg [pPtrLength-1:0] ptr;
-
-always @(posedge clk)
-begin:ptr_update
-    if (!reset_n)
-        ptr <= {pPtrLength{1'd0}};
-    else if (enable)
+generate
+if (pDelay < 1)
+begin:pDelay_assert
+    initial
     begin
-        if (ptr == pDelay - 2)
-            ptr <= {pPtrLength{1'd0}};
-        else
-            ptr <= ptr + 1;
-        val_out <= data[ptr];
-        data[ptr] <= val_in;
+        $display("pDelay is less than 1.");
+        $finish();
     end
 end
+else if (pDelay == 1)
+begin:sync_reg_gen
+    // register
+    always @(posedge clk)
+    begin:sync_reg_update
+        if (!reset_n)
+            val_out <= {pDataLength{1'd0}};
+        else if (enable)
+            val_out <= val_in;
+    end
+end
+else if (pDelay > 1)
+begin:shift_reg_gen
+    reg [pDataLength-1:0] data[pDelay-2:0];
+    reg [pPtrLength-1:0] ptr;
+    // shift register
+    always @(posedge clk)
+    begin:ptr_update
+        if (!reset_n)
+            ptr <= {pPtrLength{1'd0}};
+        else if (enable)
+        begin
+            if (ptr == pDelay - 2)
+                ptr <= {pPtrLength{1'd0}};
+            else
+                ptr <= ptr + 1;
+            val_out <= data[ptr];
+            data[ptr] <= val_in;
+        end
+    end
+end
+endgenerate
 
 endmodule
